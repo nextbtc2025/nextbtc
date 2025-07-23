289b22fcc4e1c0e7e65c63d784d2f4ae8cd32499
btcdage2013 <btcdage@proton.me>
2025-07-24 00:37:03 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -29,7 +29,7 @@ static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits
 {
     const char* pszTimestamp = "From values, perception is born. From perception, judgment arises. From judgment, choice is made. From choice, destiny unfolds.";
     CMutableTransaction txNew;
-    txNew.version = 1; 
+    txNew.version = 1;
     txNew.vin.resize(1);
     txNew.vout.resize(1);
     txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -53,17 +53,17 @@ public:
         m_chain_type = ChainType::MAIN;
         consensus.nSubsidyHalvingInterval = 210000;
 
-        consensus.BIP34Height = 1; 
-        consensus.BIP65Height = 1; 
-        consensus.BIP66Height = 1; 
-        consensus.CSVHeight = 1;   
-        consensus.SegwitHeight = 0; 
+        consensus.BIP34Height = 1;
+        consensus.BIP65Height = 1;
+        consensus.BIP66Height = 1;
+        consensus.CSVHeight = 1;
+        consensus.SegwitHeight = 0;
 
-        consensus.powLimit = uint256("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); 
-        consensus.nPowTargetTimespan = 3 * 60 * 60; 
-        consensus.nPowTargetSpacing = 5 * 60; 
-        consensus.fPowAllowMinDifficultyBlocks = true; 
-        consensus.fPowNoRetargeting = false; 
+        consensus.powLimit = uint256("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.nPowTargetTimespan = 3 * 60 * 60;
+        consensus.nPowTargetSpacing = 5 * 60;
+        consensus.fPowAllowMinDifficultyBlocks = true;
+        consensus.fPowNoRetargeting = false;
 
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = Consensus::BIP9Deployment::NEVER_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
@@ -73,27 +73,31 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].min_activation_height = 0;
 
-        genesis = CreateGenesisBlock(1753258714, 255602, 0x1f00ffff, 1, 50 * COIN); 
+        genesis = CreateGenesisBlock(1368374520, 31789, 0x1f00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
 
-        assert(consensus.hashGenesisBlock == uint256("00003bc4a3307f5f082587474bc55258d4b8504bb54ed1e7434ad6f3cc25e453"));
-        assert(genesis.hashMerkleRoot == uint256("ae41db9e1127f666272fa489303c037d3ba5573d4801312d0c5e26dc46fd351a"));
+        assert(consensus.hashGenesisBlock == uint256("0000ac4cf305a3c4add5167ed3d926ab4074fb129b05f848fb902fec16012405"));
+        assert(genesis.hashMerkleRoot == uint256("3a5b7fb5aba81bb4ee6e54cd8fbeacf9912ae8c5d80aa746a4c2d0cebe0a3a96"));
+
+        vSeeds.clear();
+        vFixedSeeds.clear();
+        vFixedSeeds.emplace_back("122.51.252.52");
 
         pchMessageStart[0] = 0xda;
-        pchMessageStart[1] = 0x9e; 
+        pchMessageStart[1] = 0x9e;
         pchMessageStart[2] = 0xb5;
         pchMessageStart[3] = 0xda;
-        nDefaultPort = 8555; 
+        nDefaultPort = 8555;
         nPruneAfterHeight = 1000;
-        m_assumed_blockchain_size = 1; 
-        m_assumed_chain_state_size = 1; 
+        m_assumed_blockchain_size = 1;
+        m_assumed_chain_state_size = 1;
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0); 
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5); 
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128); 
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E}; 
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4}; 
-        bech32_hrp = "bc"; 
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        bech32_hrp = "bc";
     }
 };
 
